6ec0d14ac1abbe039828eab0c309c10cd865fd8f
naga10 <someswarao10@gmail.com>
2017-08-30 15:37:21 -0400

    Add project files.

---
+++ Fibonacci_series/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1,temp;
+            Console.Write("Enter the number to limit the series:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a);
+            for (int i = 0; b <= c; i++)
+            {
+                Console.Write(" "+b);
+                temp = b + a;
+                a = b;
+
+                b = temp;
+            }
+            Console.ReadLine();
+        }
+    }
+}
